90f5842b021c699ed832e59681b34f88f7d92af1
Dominik Noszlopi <61868789+NoszDom@users.noreply.github.com>
2026-01-15 15:52:32 +0100

    refactor: remove config screen entrypoint (#3)

--- src/index.tsx
+++ src/index.tsx
@@ -7,38 +7,26 @@ import {
 import "datocms-react-ui/styles.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import ConfigScreen from "./entrypoints/ConfigScreen";
 import { CustomPanel } from "./entrypoints/CustomSidebarPanel";
-import { render } from "./utils/render";
 
 const CUSTOM_SIDEBAR_ID = "sidebarRecordCopyEnv";
 
 connect({
   itemFormSidebarPanels(model: ModelBlock, ctx: IntentCtx) {
-    const { modelApiKey } = ctx.plugin.attributes.parameters;
-    const keys = (modelApiKey as string)?.split(",");
-    if (keys?.includes(model.attributes.api_key))
-      return [
-        {
-          id: CUSTOM_SIDEBAR_ID,
-          label: "Copy record",
-          startOpen: true,
-        },
-      ];
-    return [];
+    return [
+      {
+        id: CUSTOM_SIDEBAR_ID,
+        label: "Copy record",
+        startOpen: true,
+      },
+    ];
   },
-  renderItemFormSidebarPanel(
-    sidebarPanelId,
-    ctx: RenderItemFormSidebarPanelCtx
-  ) {
+  renderItemFormSidebarPanel(_, ctx: RenderItemFormSidebarPanelCtx) {
     ReactDOM.render(
       <React.StrictMode>
         <CustomPanel ctx={ctx} />
       </React.StrictMode>,
       document.getElementById("root")
     );
-  },
-  renderConfigScreen(ctx) {
-    return render(<ConfigScreen ctx={ctx} />);
   },
 });
